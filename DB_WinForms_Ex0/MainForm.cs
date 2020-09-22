7d8e943176a83a8a3f7c41a240174352f18de296
Maksimov-Git <maksimov_alexey@inbox.ru>
2020-09-22 19:23:12 +0300

    коммит

---
+++ DB_WinForms_Ex0/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//
+using System.Data.SqlClient;
+
+namespace DB_WinForms_Ex0
+{
+    
+
+
+    public partial class MainForm : Form
+    {
+        public DBLoginForm LoginForm;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+        private void dataBaseConnectionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+           // sqlConnectionString = new SqlConnectionStringBuilder();
+            LoginForm = new DBLoginForm();
+            LoginForm.Owner = this;
+            LoginForm.ShowDialog();
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SqlConBuilder.Command.CommandText = "SELECT TOP(1000) *FROM[TSOOPEX].[Person].[Address]";
+
+            if (SqlConBuilder.Read() == true)
+            {
+                MessageBox.Show("чтение прошло успешно");
+            
+            }
+            else
+            {
+                MessageBox.Show(" что-то пошло не так! ");
+            }
+        }
+    }
+}
---
+++ DB_WinForms_Ex0/SQLConBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+namespace DB_WinForms_Ex0
+{
+    public static class SqlConBuilder
+    {
+
+        public static SqlConnectionStringBuilder SqlConnectionString;
+        public static SqlConnection Connection;
+        public static SqlCommand Command;
+        public static SqlDataAdapter SqlAdapter;
+        public static SqlDataReader Reader;
+        public static Boolean ConnectionTest()
+        {
+            
+
+            Connection.ConnectionString = SqlConnectionString.ConnectionString;
+            try
+            {
+                Connection.Open();
+                return true;
+            }
+            catch (Exception ex) // 
+            {
+                return false;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+
+
+
+
+
+        }
+        static SqlConBuilder()
+        {
+            Connection = new SqlConnection();
+            SqlConnectionString = new SqlConnectionStringBuilder();
+            SqlConnectionString.DataSource = "tsoop.c6oo9thiejqr.us-east-1.rds.amazonaws.com";
+            SqlConnectionString.InitialCatalog = "TSOOPEX";
+
+            Command = new SqlCommand("", Connection);
+        }
+        public static Boolean Read()
+        {
+            try
+            {
+                Connection.Open();
+                Reader = Command.ExecuteReader();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
+    }
+}
